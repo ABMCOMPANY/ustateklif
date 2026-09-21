b2ea83e5a6e3ed5e3bbd2d016a0b25fbfb82b328
ABMCOMPANY <baro330133@gmail.com>
2026-09-21 09:02:16 +0300

    Bump Tamİşim service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Basit çevrimdışı kabuk: önce ağ, olmazsa önbellek.
-const CACHE = 'ustateklif-v9';
+const CACHE = 'tamisim-v10';
 const SHELL = ['./', 'index.html', 'config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
