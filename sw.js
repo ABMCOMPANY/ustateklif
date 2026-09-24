29299d8b6c28a68df439a0b06175e443b2cc79b6
ABMCOMPANY <baro330133@gmail.com>
2026-09-24 16:12:36 +0300

    Refresh cache for rebuilt listing search

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Basit çevrimdışı kabuk: önce ağ, olmazsa önbellek.
-const CACHE = 'tamisim-v20';
+const CACHE = 'tamisim-v21';
 const SHELL = ['./', 'index.html', 'config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
