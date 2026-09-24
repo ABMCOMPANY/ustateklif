b6a6e43941dd28447efd764e7b2db0669ff5304f
ABMCOMPANY <baro330133@gmail.com>
2026-09-24 16:54:46 +0300

    Refresh cache for quote flow polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Basit çevrimdışı kabuk: önce ağ, olmazsa önbellek.
-const CACHE = 'tamisim-v23';
+const CACHE = 'tamisim-v24';
 const SHELL = ['./', 'index.html', 'config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
