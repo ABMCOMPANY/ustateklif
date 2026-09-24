a74a6c4b843fa9e507c36f864aefda875c286c7e
ABMCOMPANY <baro330133@gmail.com>
2026-09-24 07:02:55 +0300

    Refresh cache for expert panel contrast fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Basit çevrimdışı kabuk: önce ağ, olmazsa önbellek.
-const CACHE = 'tamisim-v13';
+const CACHE = 'tamisim-v14';
 const SHELL = ['./', 'index.html', 'config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
