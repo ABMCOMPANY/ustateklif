4b0ac552a0f95f6d916a8f082ddc612831bf5e42
ABMCOMPANY <baro330133@gmail.com>
2026-09-25 19:51:34 +0300

    Bump cache for guest conversion redesign

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Basit çevrimdışı kabuk: önce ağ, olmazsa önbellek.
-const CACHE = 'tamisim-v43';
+const CACHE = 'tamisim-v44';
 const SHELL = ['./', 'index.html', 'config.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
